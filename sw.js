00f8be3b56edf64948cecd4e74b37e41d5039c79
Itero Proto <itero.proto@gmail.com>
2025-11-27 21:35:05 +0300

    Test-TrudovoePravo

--- sw.js
+++ sw.js
@@ -2,12 +2,12 @@ self.addEventListener("install", event => {
   event.waitUntil(
     caches.open("app-cache-v1").then(cache => {
       return cache.addAll([
-        "/Test-Uridicheskoe_litso/index.html",
-        "/Test-Uridicheskoe_litso/manifest.json",
-        "/Test-Uridicheskoe_litso/sw.js",
-        "/Test-Uridicheskoe_litso/styles.css",
-        "/Test-Uridicheskoe_litso/icons/icon-192.png",
-        "/Test-Uridicheskoe_litso/icons/icon-512.png"
+        "/Test-TrudovoePravo/index.html",
+        "/Test-TrudovoePravo/manifest.json",
+        "/Test-TrudovoePravo/sw.js",
+        "/Test-TrudovoePravo/styles.css",
+        "/Test-TrudovoePravo/icons/icon-192.png",
+        "/Test-TrudovoePravo/icons/icon-512.png"
       ]);
     })
   );
